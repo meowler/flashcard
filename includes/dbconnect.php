d9c15a5f3fa588d3c8f7bb1c28ccdfdf1bd4a828
Lindsey Fowler <lfowler1290@gmail.com>
2015-12-15 10:42:33 -0500

    Deletes HTML files. Updates connect to 'flashcard' db includes with edits to host, username and password for remote connection.

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,8 +1,12 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pwd = "";
-$database = "flashcard";
+//$host = "localhost";
+//$user = "root";
+//$pwd = "";
+//$database = "flashcard";
+$host = "mysql.lindseyfowler.com";
+$user = "flashcardUser";
+$pwd = "flashcardPwd";
+$database = "lindseyfowler_flashcards";
 
 $connection = new PDO("mysql:host=$host;dbname=$database;", $user, $pwd);
